fc12f07ca8bcaa78d646157118febfca08140970
jiangyuxu <jiang.yuxu@mech-mind.net>
2023-02-28 15:46:31 +0800

    feat: 提交celery的学习初版

--- App.py
+++ App.py
@@ -12,7 +12,7 @@ app = Celery()
 
 app.config_from_object('celeryconfig')  # 只要在当前目录中有celeryconfig.py模块就可以加载
 
-app.tasks.register(AddTask())
+app.task.register(AddTask())
 
 if __name__ == '__main__':
     app.start()
